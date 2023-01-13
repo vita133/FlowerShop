25983c51fccd78cddc9165ffaf8b491b7038aa20
vita133 <vitandrukhiv@gmail.com>
2023-01-13 18:29:55 +0200

    added

--- app/src/main/java/com/example/flowershop/ContentActivity.kt
+++ app/src/main/java/com/example/flowershop/ContentActivity.kt
@@ -1,16 +1,23 @@
 package com.example.flowershop
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.flowershop.databinding.ActivityContentBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ContentActivity : AppCompatActivity() {
     private lateinit var binding: ActivityContentBinding
+    private val database = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityContentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val item = intent.getSerializableExtra("item") as Product
         binding.apply {
             Glide.with(this@ContentActivity)
@@ -18,7 +25,24 @@ class ContentActivity : AppCompatActivity() {
                 .into(imageView)
             nameTextView.text = item.name
             priceContent.text = item.price.toString()
+        }
+        binding.favoriteButton2.setOnClickListener(){
+            val id = item.id.toString()
+            val name = item.name
+            val price = item.price
+            val image = item.image
 
+            val infoProduct = HashMap<String,Any>()
+            infoProduct.put("name", name!!)
+            infoProduct.put("price", price!!)
+            infoProduct.put("image", image!!)
+            database.collection("favorite").document(id).set(infoProduct).addOnSuccessListener{
+                Toast.makeText(this, "Product to favorite saved", Toast.LENGTH_SHORT).show();
+            }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show();
+                    Log.d(ContentValues.TAG, it.toString());
+                }
         }
     }
 }
